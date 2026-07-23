cb87f13ff19f12b961a1a1e525d1ef1cb4dcd9d4
Artur Karczmarczyk <artur@ideaspot.pl>
2026-07-23 13:22:13 +0200

    #20 export tests

--- tests/exports.test.ts
+++ tests/exports.test.ts
@@ -10,6 +10,14 @@ describe("MCDA Exports", () => {
     expect(mcda.PrometheeDecisionProblem).toBeDefined();
   });
 
+  test("should export VikorDecisionProblem", () => {
+    expect(mcda.VikorDecisionProblem).toBeDefined();
+  });
+
+  test("should export SpotisDecisionProblem", () => {
+    expect(mcda.SpotisDecisionProblem).toBeDefined();
+  });
+
   test("should export MabacDecisionProblem", () => {
     expect(mcda.MabacDecisionProblem).toBeDefined();
   });
