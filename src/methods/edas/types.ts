946a075b12996951ce27b1aa2612649f903bb427
nicku3d <nicku3d@gmail.com>
2026-07-24 14:42:14 +0200

    #6 edas implementation

---
+++ src/methods/edas/types.ts
@@ -0,0 +1,10 @@
+export type EdasDebugResults = {
+  averageSolution: number[];
+  positiveDistanceMatrix: number[][];
+  negativeDistanceMatrix: number[][];
+  weightedPositiveDistances: number[];
+  weightedNegativeDistances: number[];
+  normalizedPositiveDistances: number[];
+  normalizedNegativeDistances: number[];
+  appraisalScores: number[];
+};
